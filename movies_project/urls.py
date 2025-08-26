75766717bf0849467c5f2f5c156824521678a690
emansvt <emanmahmoudsvt@gmail.com>
2025-08-26 17:45:21 +0300

    Day 2 – Serializers & CRUD Endpoints

--- movies_project/urls.py
+++ movies_project/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from movie_api.api.views import ReviewViewSet   # import from correct app
-
-router = DefaultRouter()
-router.register(r'reviews', ReviewViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
 ]
-
